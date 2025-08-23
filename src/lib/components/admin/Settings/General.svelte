356e3fd4afcd005884e80cb83a296efd71e1f185
Carlos Delfino <consultoria@carlosdelfino.eti.br>
2025-08-23 11:07:39 -0300

    Arquivos que ficaram faltando

--- src/lib/components/admin/Settings/General.svelte
+++ src/lib/components/admin/Settings/General.svelte
@@ -141,7 +141,7 @@
 
 									{#if $config?.features?.enable_version_update_check}
 										<a
-											href="https://github.com/open-webui/open-webui/releases/tag/v{version.latest}"
+											href="https://github.com/rapporttecnologia/rapport-generativa/releases/tag/v{version.latest}"
 											target="_blank"
 										>
 											{updateAvailable === null
@@ -214,7 +214,7 @@
 									/>
 								</a>
 
-								<a href="https://github.com/open-webui/open-webui" target="_blank">
+								<a href="https://github.com/rapporttecnologia/rapport-generativa" target="_blank">
 									<img
 										alt="Github Repo"
 										src="https://img.shields.io/github/stars/open-webui/open-webui?style=social&label=Star us on Github"
--- src/lib/components/layout/Sidebar/UserMenu.svelte
+++ src/lib/components/layout/Sidebar/UserMenu.svelte
@@ -173,7 +173,7 @@
 						on:click={() => {
 							show = false;
 						}}
-						href="https://github.com/rapport/rapport-generativa/releases"
+						href="https://github.com/rapporttecnologia/rapport-generativa/releases"
 					>
 						<Map className="size-5" />
 						<div class="flex items-center">{$i18n.t('Releases')}</div>
